b9ac94e12240f912aed5c7e044972763a99f68de
SirawatMT <matteh.2014@gmail.com>
2026-02-28 01:24:50 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE = 'kheeleum-v1';
+const CACHE = 'kheeleum-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Install — cache all assets
